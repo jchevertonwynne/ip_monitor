ec6f3d7d15b7d6c7bc77361febc1eaf5bbd2223e
Joseph Cheverton-Wynne <jchevertonwynne@gmail.com>
2024-09-15 17:00:55 +0100

    auto update the conf file

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use anyhow::Context;
 
 const PATH: &str = "/home/jcw/projects/my_ip/README.md";
+const SETUP_VARS: &str = "/etc/pivpn/wireguard/setupVars.conf";
 
 fn main() -> anyhow::Result<()> {
     let external_ip = get_external_ip()?;
@@ -13,7 +14,8 @@ fn main() -> anyhow::Result<()> {
         return Ok(());
     }
 
-    update_stored_ip(external_ip)?;
+    update_stored_ip(&external_ip)?;
+    update_conf(&external_ip)?;
 
     Ok(())
 }
@@ -30,7 +32,7 @@ fn get_stored_ip() -> anyhow::Result<String> {
     std::fs::read_to_string(PATH).context("failed to read stored ip file")
 }
 
-fn update_stored_ip(ip: String) -> anyhow::Result<()> {
+fn update_stored_ip(ip: &str) -> anyhow::Result<()> {
     std::fs::write(PATH, ip.as_bytes()).context("failed to update the stored ip file")?;
 
     println!("updated README.md");
@@ -67,5 +69,18 @@ fn update_stored_ip(ip: String) -> anyhow::Result<()> {
     )?;
     println!("{out}");
 
+    Ok(())
+}
+
+fn update_conf(ip: &str) -> anyhow::Result<()> {
+    let context_vars =
+        std::fs::File::open(SETUP_VARS).context("failed to open context vars file")?;
+    let mut context_vars = conf_parser::processer::read_to_file_conf(&context_vars)
+        .context("failed to read context vars")?;
+    if let Some(section) = context_vars.sections.get_mut("*placeholder*") {
+        section.set_property("pivpnHOST", ip);
+    }
+    conf_parser::processer::write_to_file(&context_vars, SETUP_VARS)
+        .context("failed to write new conf")?;
     Ok(())
 }
